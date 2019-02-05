48aae64c2fd4f8610cfe1243e9cd407ef59fe6ba
Dan <headhertz88@gmail.com>
2019-02-05 17:02:16 +0000

    Prototype test

---
+++ test/prototype.test.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect;
+const scout_prototype = require('../prototype/scout_prototype');
+
+describe('prototype test', () => {
+	it('clone', () => {
+		let ryan = scout_prototype.clone();
+		ryan.name = 'Ryan Ford';
+		ryan.addItemToList('reading light');
+
+		let sally = scout_prototype.clone();
+		sally.name = 'Sally Ford';
+		sally.addItemToList('reading light');
+
+		let andrew = scout_prototype.clone();
+		andrew.name = 'Andrew Ford';
+		andrew.addItemToList('cheese wegde');
+
+		expect(ryan.name === 'Ryan Ford').to.be.true;
+		expect(ryan.shoppingList === sally.shoppingList).to.be.true;
+		expect(sally.shoppingList === andrew.shoppingList).to.be.false;
+	});
+});
